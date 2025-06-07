00d193d8d57b2a452ec320aceeae3461bbd968b8
Riquey654 <riqueyukio@gmail.com>
2025-06-07 10:00:21 -0300

    socket good

---
+++ src/socketUDP.hpp
@@ -0,0 +1,13 @@
+#ifndef SOCKETUDP_H
+#define SOCKETUDP_H
+
+#include <string>
+#include <cstddef>
+#include <cstdint>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void createSocket(int packet, string hostname, int port);
+
+#endif
