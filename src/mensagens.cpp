e62e0a60ece5c06bcb34345345188a714dd0b555
Riquey654 <riqueyukio@gmail.com>
2025-06-07 11:33:46 -0300

    a

--- src/mensagens.cpp
+++ src/mensagens.cpp
@@ -40,6 +40,9 @@ PacoteSlow Disconnect(bitset<128> uuid, bitset<27> sttl, uint32_t seqnum, uint32
 
     return pacote;
 }
+
+
+
 PacoteSlow sendData(bitset<128> uuid, bitset<27> sttl, uint32_t ultimoSeqNum, 
 uint32_t ultimoAckNum, uint16_t window, bool maisDados, vector<uint8_t> data) {
     /**
@@ -96,9 +99,6 @@ uint32_t ultimoAckNum, uint16_t window, bool maisDados, vector<uint8_t> data) {
 
     return pacote;
 }
-
-
-using namespace std;
 
 
 PacoteSlow connect(uint16_t tamBufferRecebimento) {
