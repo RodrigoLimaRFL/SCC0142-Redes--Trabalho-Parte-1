be2607d1b3059a885196ef9e3e340a658e139d37
Riquey654 <riqueyukio@gmail.com>
2025-06-07 11:31:59 -0300

    connect

--- src/mensagens.cpp
+++ src/mensagens.cpp
@@ -1,1 +1,26 @@
 #include "mensagens.hpp"
+
+using namespace std;
+
+
+PacoteSlow connect(uint16_t tamBufferRecebimento) {
+
+    PacoteSlow pacoteConnect;
+
+    // NIL UUID
+    // STTL 0
+
+    bitset<5> flags('00001'); // flags: connect(1)
+    pacoteConnect.setFlags(flags);
+
+    //seqNum 0
+    // ackNum 0
+
+    pacoteConnect.setWindow(tamBufferRecebimento); // window
+
+    // fid 0
+    // fo 0
+    // data.clear
+
+    return pacoteConnect;
+}
