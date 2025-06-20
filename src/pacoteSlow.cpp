d23f7d3f0f525e431d3f7ca8712b35838e7c20d2
Rodrigo Lima <guico21@gmail.com>
2025-06-20 10:01:40 -0300

    Update pacoteSlow.cpp

--- src/pacoteSlow.cpp
+++ src/pacoteSlow.cpp
@@ -58,7 +58,7 @@ bool PacoteSlow::setFo(uint8_t newFo) {
     return true;
 }
 
-bool PacoteSlow::setData(const vector<uint8_t>& newData, int numBytes) {
+bool PacoteSlow::setData(const vector<uint8_t>& newData, size_t numBytes) {
     if(numBytes < 0 || numBytes > TAMANHO_MAXIMO_DADOS) {
         cerr << "Erro: Tamanho de dados inválido. Deve ser entre 0 e 1440 bytes." << endl;
         return false;
